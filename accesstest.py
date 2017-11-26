4a471eebf3bcaf40518fe676d3a1b7b688969c1b
Klaus Bremer <bremer@bremer-media.de>
2017-11-26 16:24:56 +0100

    minor rewrite, quicktest.py removed

--- accesstest.py
+++ accesstest.py
@@ -1,31 +1,69 @@
 
+import sys
 from pprint import pprint
+
+from fuzzywuzzy import fuzz
 from youtubeservice import YouTubeService
+
+
+def get_channels(service, channel_name):
+    results = service.search().list(
+        part='id, snippet', #,contentDetails,statistics',
+        type='channel',
+        q=channel_name,
+    ).execute()
+    items = results['items']
+    pprint(items)
+
+
 
 
 def test():
     print('\n\n=== OUTPUT START ===\n')
     ys = YouTubeService()
     service = ys.service
+    channels = get_channels(service, channel_name='PyDDF')
+    pprint(channels)
+    print('\n\n=== FINISH ===\n')
+
+
+def x_test(channel_name='EuroPython Conference'):
+    print('\n\n=== OUTPUT START ===\n')
+    ys = YouTubeService()
+    service = ys.service
     results = service.search().list(
         part='id, snippet', #,contentDetails,statistics',
         type='channel',
-        q='EuroPython Conference',
+        q=channel_name,
     ).execute()
-    pprint(results)
+    #pprint(results)
     data = results['items']
     for entry in range(len(results['items'])):
         data = results['items'][entry]
         snippet = data['snippet']
-        pprint(data['id'])
-        pprint(snippet['title'])
+        channelId = data['id']['channelId']
+        title = snippet['title']
+
+        print('title: {}, channelId: {}'.format(title, channelId))
+
+        videos = service.search().list(
+            part='id, snippet', #,contentDetails,statistics',
+            type='video',
+            q=channelId,
+        ).execute()
+        pprint(videos)
+
+
 #
 #         pprint(results['items'][entry]['snippet']['title'])
 #         pprint(results['items'][entry]['snippet']['channelTitle'])
 #     pprint(elements)
 #    elements = results['items'][0]['id']
 #     pprint(channelID)
+
+
     print('\n\n=== FINISH ===\n')
 
+
 if __name__ == '__main__':
-    test()
+    test()  #sys.argv[1])
